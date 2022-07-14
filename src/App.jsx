264ee86252a45c73730589890ee81e4e3b036b12
Valenthina Temudo <temudo.valenthina@gmail.com>
2022-07-14 16:30:21 -0300

    sidebar pronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
-import './styles.css'
+import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post  
-        author="Valenthina Temudo" 
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi natus quia nihil error minima accusamus perspiciatis, dolore quasi reiciendis cum possimus quibusdam aliquam laudantium officiis exercitationem itaque beatae, impedit debitis." 
-      />
-      <Post
-        author="Diego Fernandes"
-        content="ola mundo"
-      />
+
+      <div className={styles.wrapper}>
+       <Sidebar />
+        <main>
+          <Post  
+            author="Valenthina Temudo" 
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi natus quia nihil error minima accusamus perspiciatis, dolore quasi reiciendis cum possimus quibusdam aliquam laudantium officiis exercitationem itaque beatae, impedit debitis." 
+          />
+          <Post
+            author="Diego Fernandes"
+            content="ola mundo"
+          />
+        </main>
+      </div>
     </div>
   )
 }
